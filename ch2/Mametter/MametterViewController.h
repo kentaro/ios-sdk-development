59337681794f541fd8a98cf1c1cc227d71ea0520
Kentaro Kuribayashi <kentarok@gmail.com>
2012-09-10 21:03:44 +0900

    Add files for ch2.

---
+++ ch2/Mametter/MametterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MametterViewController.h
+//  Mametter
+//
+//  Created by usr0600239 on 2012/08/28.
+//  Copyright (c) 2012年 kentarok.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MametterViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIWebView *twitterWebView;
+
+@end
---
+++ ch2/Mametter/MametterViewController.m
@@ -0,0 +1,55 @@
+//
+//  MametterViewController.m
+//  Mametter
+//
+//  Created by usr0600239 on 2012/08/28.
+//  Copyright (c) 2012年 kentarok.org. All rights reserved.
+//
+
+#import "MametterViewController.h"
+#import <Twitter/Twitter.h>
+
+@interface MametterViewController ()
+
+@end
+
+@implementation MametterViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [self setTwitterWebView:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (IBAction)handleTweetButtonTapped:(id)sender
+{
+    if ([TWTweetComposeViewController canSendTweet]) {
+        TWTweetComposeViewController *tweetVC =
+        [[TWTweetComposeViewController alloc] init];
+        [tweetVC setInitialText: NSLocalizedString(
+            @"I just finished the first project in iOS SDK Development. #pragsios", nil
+        )];
+        [self presentViewController:tweetVC animated:YES completion:NULL]; }
+    else
+    {
+        NSLog (@"Can't send tweet");
+    }
+}
+
+- (IBAction) handleShowMyTweetsTapped: (id) sender {
+    [self.twitterWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.twitter.com/kentaro"]]];
+}
+
+@end
